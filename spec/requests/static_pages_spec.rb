eb3ad3784ba39b193dd6c3d40b24640e618c4a1c
Bernd Kolb <bernd.kolb@gmail.com>
2014-08-11 19:42:25 +0200

    refactored tests, added signup page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,64 +2,35 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Flugplanung')
-    end
+    it { should have_content('Flugplanung') }
+    it { should have_title(full_title('')) }
+    it {should_not have_title(" | Home") }
   end
-
-  it "should have the right title" do
-    visit '/static_pages/home'
-    expect(page).to have_title("Flugplanung | Home")
-  end
-
-
-
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Hilfe')
-    end
+    it { should have_content('Hilfe') }
+    it { should have_title(full_title('Hilfe')) }
   end
-
-  it "should have the right title" do
-    visit '/static_pages/help'
-    expect(page).to have_title("Flugplanung | Hilfe")
-  end
-
-
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('Über uns')
-    end
+    it { should have_content('Über uns') }
+    it { should have_title(full_title('Über uns')) }
   end
 
-  it "should have the right title" do
-    visit '/static_pages/about'
-    expect(page).to have_title("Flugplanung | Über uns")
+  describe "About page" do
+    before { visit contactus_path }
+
+    it { should have_content('Kontakt zu uns') }
+    it { should have_title(full_title('Kontakt zu uns')) }
   end
-
-
-
-  describe "Contact page" do
-
-    it "should have the content 'Kontakt zu uns'" do
-      visit '/static_pages/contactus'
-      expect(page).to have_content('Kontakt zu uns')
-    end
-  end
-
-  it "should have the right title" do
-    visit '/static_pages/contactus'
-    expect(page).to have_title("Flugplanung | Kontakt zu uns")
-  end
-
 
 end
