0a867693bbfc328e36b25665c18140ecb7e6336f
Bernd Kolb <bernd.kolb@gmail.com>
2014-08-11 20:03:25 +0200

    simplified the tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,33 +4,60 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Flugplanung') }
-    it { should have_title(full_title('')) }
+    let(:title) { '' }
+    let(:heading) { 'Flugplanung' }
+
+    it_should_behave_like "all static pages"
     it {should_not have_title(" | Home") }
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Hilfe') }
-    it { should have_title(full_title('Hilfe')) }
+    let(:title) { 'Hilfe' }
+    let(:heading) { 'Hilfe' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_content('Über uns') }
-    it { should have_title(full_title('Über uns')) }
+    let(:title) { 'Über uns' }
+    let(:heading) { 'Über uns' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit contactus_path }
 
-    it { should have_content('Kontakt zu uns') }
-    it { should have_title(full_title('Kontakt zu uns')) }
+    let(:title) { 'Kontakt zu uns' }
+    let(:heading) { 'Kontakt zu uns' }
+
+    it_should_behave_like "all static pages"
+  end
+
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "Über uns"
+    expect(page).to have_title(full_title('Über uns'))
+    click_link "Hilfe"
+    expect(page).to have_title(full_title('Hilfe'))
+    click_link "Kontakt"
+    expect(page).to have_title(full_title('Kontakt zu uns'))
+    click_link "Home"
+    click_link "Anmelden"
+    expect(page).to have_title(full_title('Anmelden'))
   end
 
 end
